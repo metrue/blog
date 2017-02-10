9e98b417e8c1d25e6da4e393c216753f675a0e8e
metrue <h.minghe@gmail.com>
2017-02-10 17:31:01 +0800

    deploy

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -4,8 +4,8 @@ host='root@139.162.72.153'
 ssh ${host} <<END
   docker rmi \$(docker images --filter "dangling=true" -q --no-trunc)
   rm -rf /tmp/asmalltalk.com
-  git clone git@github.com:metrue/asmalltalk.com.git /tmp/asmalltalk.com
-  cd /tmp/asmalltalk.com
+  git clone git@github.com:metrue/metrue.github.io.git /tmp/minghe.me
+  cd /tmp/minghe.me
   docker build -f devops/dockerfile.nginx -t seal .
   docker run -d -p 80:80 seal
 END

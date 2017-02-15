02479a5366a6f57bd4909bce0d27e58b57e4b38c
metrue <h.minghe@gmail.com>
2017-02-15 16:57:46 +0800

    try to support https

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -6,6 +6,13 @@ ssh ${host} <<END
   rm -rf /tmp/minghe.me
   git clone git@github.com:***REMOVED***/***REMOVED***.github.io.git /tmp/minghe.me
   cd /tmp/minghe.me
+
+	docker run --rm -p 80:80 -p 443:443 \
+    -v /etc/letsencrypt:/etc/letsencrypt \
+    quay.io/letsencrypt/letsencrypt auth \
+    --standalone -m h.minghe@gmail.com --agree-tos \
+    -d minghe.me
+
   docker build -f devops/dockerfile.nginx -t seal .
-  docker run -d -p 80:80 seal
+  docker run -d -p 80:80 -v /etc/letsencrypt:/etc/letsencrypt seal
 END

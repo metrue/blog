ed1b76ff39045a61262332d775dd4a08f2b5fc8b
metrue <h.minghe@gmail.com>
2017-04-17 12:05:58 +0800

    mount let's encrypt stuff

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -7,7 +7,7 @@ ssh ${target} <<END
   git clone https://github.com/metrue/metrue.github.io.git /tmp/minghe.me
   cd /tmp/minghe.me
 
-  docker rm \$(docker stop \$(docker ps -a -q --filter ancestor=seal  --format="{{.ID}}"))
+  docker rm \$(docker stop \$(docker ps -a -q --filter ancestor=metrue/cici  --format="{{.ID}}"))
 
   docker run --rm -p 80:80 -p 443:443 \
     -v /etc/letsencrypt:/etc/letsencrypt \
@@ -20,5 +20,9 @@ ssh ${target} <<END
     quay.io/letsencrypt/letsencrypt renew \
     --standalone
 
-   docker run -p 80:80 -p 443:443 -v /tmp/minghe.me/posts:/opt/cici/posts  -v /tmp/minghe.me/devops/nginx.conf:/etc/nginx/nginx.conf metrue/cici
+  docker run -p 80:80 -p 443:443  \
+    -v /tmp/minghe.me/posts:/opt/cici/posts \
+    -v /tmp/minghe.me/devops/nginx.conf:/etc/nginx/nginx.conf \
+    -v /etc/letsencrypt:/etc/letsencrypt \
+    metrue/cici
 END

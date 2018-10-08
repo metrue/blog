8938113e94cf16f3a351484f9fbef482f35b7d2b
Minghe Huang <h.minghe@gmail.com>
2018-10-08 11:14:56 +0800

    improve style

--- public/extra.js
+++ public/extra.js
@@ -3,5 +3,9 @@ window.onload = () => {
   const keys = Object.keys(eles);
   keys.forEach((k) => {
     eles[k].style.textAlign = 'justify';
+    eles[k].style.lineHeight = 1.58;
   });
+
+  document.body.style.fontFamily = '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
+  document.body.style.fontSize = '16px';
 };

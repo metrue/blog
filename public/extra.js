38fffd248bf232a2d2d3f7f158fc4512e74e24f3
Minghe Huang <h.minghe@gmail.com>
2018-10-10 09:26:32 +0800

    inject YoYo

--- public/extra.js
+++ public/extra.js
@@ -1,11 +1,36 @@
 window.onload = () => {
-  const eles = document.getElementsByTagName('p');
-  const keys = Object.keys(eles);
-  keys.forEach((k) => {
-    eles[k].style.textAlign = 'justify';
-    eles[k].style.lineHeight = 1.58;
-  });
+  const adjustStyle = () => {
+    let eles = document.getElementsByTagName('p');
+    Object.values(eles).forEach((ele) => {
+      ele.style.textAlign = 'justify';
+      ele.style.lineHeight = 1.58;
+    });
 
-  document.body.style.fontFamily = '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
-  document.body.style.fontSize = '16px';
+    eles = document.getElementsByTagName('li');
+    Object.values(eles).forEach((item) => {
+      item.style.textAlign = 'justify';
+      item.style.lineHeight = 1.58;
+    });
+
+    document.body.style.fontFamily = '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
+    document.body.style.fontSize = '16px';
+  }
+
+  const injectYoYo = () => {
+    const element = document.createElement('div');
+    element.style.maxWidth = '46em';
+    element.style.margin = 'auto';
+    element.setAttribute("id", "YoYo");
+
+    const scriptEle = document.createElement('script');
+    scriptEle.type = 'text/javascript';
+    scriptEle.src  = 'https://yoyo-client-production.s3.amazonaws.com/dist/index.js';
+
+    document.body.appendChild(element);
+    document.body.appendChild(scriptEle);
+  }
+
+
+  adjustStyle();
+  injectYoYo();
 };
